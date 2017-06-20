dcf85b037e3287525aae2fb16ea9a8d9e060f5de
AnushaDharini <anushadharinic@gmail.com>
2017-06-20 15:59:51 +0530

    Create vowelconsonant.java

---
+++ vowelconsonant.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ class vowelorconsonant
+ {  
+ public static void main(String[] args)
+{ 
+             char value;
+             Scanner s = new Scanner(System.in);
+            System.out.println("Enter the number:");
+            value= s.nextchar();
+       if(valu==a\\value==e\\Value==i\\Value==o\\Value==u)
+              { 
+                    System.out.println("Given character is Vowel");
+              }
+     else 
+              {
+                    System.out.println("Given character is Consonant");
+              }
+   
+}
+}
+
+
+
+
+
+
+
