f4179a4c44ab66eec08d8301efc2ddb20bf3fbc8
AnushaDharini <anushadharinic@gmail.com>
2017-06-20 15:13:07 +0530

    Rename vowelorconsotant.java to vowelorconsonant.java

---
+++ vowelorconsonant.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ class vowelorconsonant
+ {  
+ public static void main(String[] args)
+{ 
+             int r;
+             Scanner s = new Scanner(System.in);
+            System.out.println("Enter the number:");
+            r= s.nextInt();
+       if (a=a&&e&&i&&o&&u)
+              { 
+                    System.out.println("Given character is Vowel");
+              }
+     else 
+              {
+                    System.out.println("Given character is Constant");
+              }
+   
+}
+}
+
+
