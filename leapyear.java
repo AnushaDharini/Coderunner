943d98ee6ad9159a9b8d65eca277b7dc3f0b8708
AnushaDharini <anushadharinic@gmail.com>
2017-06-21 09:18:37 +0530

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ class leapyear
+ {  
+ public static void main(String[] args)
+   { 
+            int i;
+           for(i=1900;i<=2020;i++)
+            {  if(i%4==0)
+                   System.out.println(i);
+              }
+     }
+}  
