3caafe625834ecf3d5603757dd9c78b3ea06d241
AnushaDharini <anushadharinic@gmail.com>
2017-06-21 09:25:21 +0530

    Create Nsum.java

---
+++ Nsum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ class Nsum
+ {  
+ public static void main(String[] args)
+         { 
+                System.out.println("Enter the value of N:");
+             int n ,i,sum=0;
+             Scanner s = new Scanner(System.in);
+               n= s.nextInt();
+             for(i=0;i<=n;i++)
+                 {
+                  sum=sum+i;
+                 }
+                       System.out.println("Sum of N numbers is:" +sum);
+            }
+}
